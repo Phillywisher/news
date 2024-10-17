3d11f14df59839cd5630f02cbb0dfe099fc6d03a
Phillywisher <willfisher467@gmail.com>
2024-10-17 18:04:59 +0100

    added comment count to article body with added tests

--- models/model.js
+++ models/model.js
@@ -98,14 +98,36 @@ exports.fetchComments = (articleId) => {
 };
 exports.selectArticleById = (article_id) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+    .query(
+      `
+      SELECT 
+        articles.article_id, 
+        articles.title, 
+        articles.topic, 
+        articles.author, 
+        articles.body, 
+        articles.created_at, 
+        articles.article_img_url, 
+        articles.votes,
+        COUNT(comments.comment_id):: INT AS comment_count
+      FROM articles
+      LEFT JOIN comments ON comments.article_id = articles.article_id
+      WHERE articles.article_id = $1
+      GROUP BY articles.article_id;
+      `,
+      [article_id]
+    )
     .then((result) => {
       if (result.rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "article does not exist" });
+        return Promise.reject({
+          status: 404,
+          msg: "article does not exist",
+        });
       }
       return result.rows[0];
     });
 };
+
 exports.createComment = (id, body) => {
   const bodyArr = [body.body, 0, body.username, id];
   const comment = format(
