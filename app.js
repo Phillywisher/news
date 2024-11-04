dd7bca70c63d85248c4537d9e8e88b9b3a641eec
Phillywisher <willfisher467@gmail.com>
2024-11-04 09:12:03 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const apiRouter = require("./routes/index");
-
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
